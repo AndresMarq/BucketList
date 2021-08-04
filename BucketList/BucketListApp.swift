0fdd1889a0139d70431a780d475cad4bb217e289
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2021-08-04 12:48:46 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Andres Marquez on 2021-08-04.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
